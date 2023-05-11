fa73cba9286ef396e8e188a5403bd882da8d3414
Emi <andere@uchicago.edu>
2023-05-10 20:50:47 -0500

    changes to webpage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import gh_logo from "./github_logo.svg"
 import lin_logo from "./linkedin_logo.svg"
 import tw_logo from "./twitter_logo.svg"
 import scholar_logo from "./google_logo.svg"
+import writing_logo from "./writing.svg"
 
 function App() {
   return (
@@ -11,12 +12,10 @@ function App() {
       <img src={logo} className="App-logo" alt="logo" />
       <p class="name_title">Emilio Andere</p>
       <p class="paragraph">
-          I'm 20 years old, originally from Guadalajara, Mexico. I'm currently studying Computer Science and Mathematics at UChicago. 
-          I focus on full-stack engineering. Database, back-end, or front-end. I previously published under Professor Zhao, and presented that project at NeurIPS '22. 
-          I currently work at <a href="https://spherepay.co/">sphere</a> as a software engineer intern.
-          At Impossible Finance I built and launched their whole rewards page, front-end in typescript, and the “learn-and-earn” back-end in Golang. I am constantly 
-          adding projects to my github during hackathons, summers, or in my free time in between classes. 
-          I'm also a big fan of the future, beautiful design, music, consciousness, within many others. <br /> 
+          I am 20 years old, originally from Guadalajara, Mexico. I'm currently studying Computer Science and Mathematics at UChicago. 
+          I focus on full-stack engineering. Database, back-end, or front-end. I previously published under Professor Zhao, and presented that project at NeurIPS '22. <br /> <br /> 
+          I currently work at <a href="https://spherepay.co/">sphere</a> as a software engineer intern. <br /> <br />
+          <a href="https://mirror.xyz/emiandere.eth">This</a> is where I very sparingly and very sometimes write.
       </p>
       <div className="logos">
         <a className="gh-logo" href="https://github.com/technoabsurdist" target="_blank" rel="noreferrer">
