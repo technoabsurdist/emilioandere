708aebb9a80d33762214bb48766993ee042f300b
Emi <andere@uchicago.edu>
2023-05-25 08:53:31 -0500

    literal '.'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <p class="paragraph">
         I am 20 years old, currently live in Chicago and interning at <a href="https://spherepay.co/">Sphere</a> as a Software Engineer. <br /> I grew up in Guadalajara, Mexico and I'm currently studying Mathematics at UChicago. <br />  
         I'm generally focused on full-stack software engineering. <br /> 
-        Some of my interests are mathematics, people, great software, and zeitgeist-altering products<br />
+        Some of my interests are mathematics, people, great software, and zeitgeist-altering products.<br />
       </p>
       <p class="paragraph">
         Email: <a href="mailto:andere@uchicago.edu">andere@uchicago.edu</a>
