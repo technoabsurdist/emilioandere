0e510c55c812571d0bf6bb2a7109b32e846bff9d
Emi <andere@uchicago.edu>
2023-05-24 20:59:51 -0500

    changes to minimalistic page instead with no images and just text and hyperlinks --very web1ish

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import './App.css';
-import logo from "./logo_beth.png"
-import gh_logo from "./github_logo.svg"
-import lin_logo from "./linkedin_logo.svg"
-import tw_logo from "./twitter_logo.svg"
-import scholar_logo from "./google_logo.svg"
 
 function App() {
   return (
     <>
-      <img src={logo} className="App-logo" alt="logo" />
       <p class="name_title">Emilio Andere</p>
       <p class="paragraph">
-          I am 20 years old, originally from Guadalajara, Mexico. I'm currently studying Computer Science and Mathematics at UChicago. 
-          I focus on full-stack engineering. Database, back-end, or front-end. I previously published under Professor Zhao, and presented that project at NeurIPS '22. <br /> <br /> 
-          I currently work at <a href="https://spherepay.co/">sphere</a> as a software engineer intern. <br /> <br />
-          <a href="https://mirror.xyz/emiandere.eth">This</a> is where I very sparingly and very sometimes write.
+        I am 20 years old, currently live in Chicago and interning at <a href="https://spherepay.co/">Sphere</a> as a Software Engineer. <br /> I grew up in Guadalajara, Mexico and I'm currently studying Mathematics at UChicago. <br />  
+        I'm generally focused on full-stack software engineering. <br /> 
+        Some of my interests are mathematics, people, great software, and zeitgeist-altering products<br />
       </p>
-      <div className="logos">
-        <a className="gh-logo" href="https://github.com/technoabsurdist" target="_blank" rel="noreferrer">
-          <img src={gh_logo} alt="logo" />
-        </a>
-        <a className="lin-logo" href="https://www.linkedin.com/in/emi-andere/" target="_blank" rel="noreferrer">
-          <img src={lin_logo} alt='logo' />
-        </a>
-        <a className="tw-logo" href="https://twitter.com/technoabsurdist" target="_blank" rel="noreferrer">
-          <img src={tw_logo} alt='logo' />
-        </a>
-        <a className="scholar-logo" href="https://scholar.google.com/citations?user=qhHsPrgAAAAJ&hl=en&authuser=1" target="_blank" rel="noreferrer">
-          <img src={scholar_logo} alt='logo' />
-        </a>
-      </div>
+      <p class="paragraph">
+        Email: <a href="mailto:andere@uchicago.edu">andere@uchicago.edu</a>
+      </p>
+      <p class="paragraph">
+        <a href="https://github.com/technoabsurdist" target="_blank" rel="noreferrer">github</a> <br />
+        <a href="https://twitter.com/technoabsurdist" target="_blank" rel="noreferrer">twitter</a> <br /> 
+        <a href="https://www.linkedin.com/in/emi-andere/" target="_blank" rel="noreferrer">linkedin</a> <br />
+        <a href="https://scholar.google.com/citations?user=qhHsPrgAAAAJ&hl=en&authuser=1" target="_blank" rel="noreferrer">scholar</a> <br /> 
+      </p>
    </>
   );
 }
